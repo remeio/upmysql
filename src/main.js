3c7b722aa6368b20d1df2dbe3327e8332ab11057
xumengqi <266347978@qq.com>
2022-04-15 23:32:10 +0800

    build: add electron

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import App from "./App.vue";
 import naive from "naive-ui";
 // 路由插件
 import { router } from "@/router/router";
+// 剪切板插件
+import VueClipboard from "vue-clipboard-plus";
+VueClipboard.config.autoSetContainer = true;
+// 代码高亮插件
+import VueHighlightJS from "vue-highlightjs";
+import "highlight.js/styles/atom-one-dark.css";
+
 // 创建 APP
 const app = createApp(App);
 // 使用插件
-app.use(naive).use(router);
+app.use(naive).use(VueHighlightJS).use(VueClipboard).use(router);
 // 挂载
 app.mount("#app");
