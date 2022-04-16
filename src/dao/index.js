ce79bdd073a0f9dd475b98d870f35dc6dff6939e
xumengqi <266347978@qq.com>
2022-04-16 13:03:51 +0800

    1

--- src/dao/index.js
+++ src/dao/index.js
@@ -10,13 +10,16 @@ var executeSql = function (sql) {
   return new Promise((resolve, reject) => {
     pool.getConnection(function (err, conn) {
       if (err) {
+        console.log("sql connection error: ", err);
         reject(err);
       } else {
         conn.query(sql, function (qerr, vals, fields) {
           pool.releaseConnection(conn);
           if (qerr) {
+            console.log("sql query error: ", err);
             reject(qerr);
           }
+          console.log("sql '" + sql + "'query success: ", vals);
           resolve(vals, fields);
         });
       }
---
+++ src/views/common/VariableList.vue
@@ -0,0 +1,53 @@
+<template>
+  <n-grid
+    :x-gap="12"
+    :y-gap="12"
+    :cols="LOCAL_CONFIG.style.gridCols"
+    style="margin: 4px"
+  >
+    <n-gi v-for="item in data" :key="item[key]">
+      <up-card
+        :label="item.name"
+        :value="valueComputed(item.value)"
+        :extra="item.extra"
+        size="small"
+        ><up-icon
+          icon="Ellipse"
+          :color="colorComputed(item.value)"
+          v-if="showSwitchComputed(item.value)" /></up-card></n-gi
+  ></n-grid>
+</template>
+
+<script>
+import { UpCard, UpIcon } from "@/components/common";
+import LOCAL_CONFIG from "@/config";
+export default {
+  components: {
+    UpCard,
+    UpIcon,
+  },
+  props: {
+    data: {
+      type: Array,
+      default: null,
+    },
+    key: {
+      type: String,
+      default: "extra",
+    },
+  },
+  setup() {
+    const colorComputed = (value) => (value == "ON" ? "green" : "red");
+    const showSwitchComputed = (value) => value == "ON" || value == "OFF";
+    const valueComputed = (value) => (value ? value : "加载中...");
+    return {
+      LOCAL_CONFIG,
+      colorComputed,
+      showSwitchComputed,
+      valueComputed,
+    };
+  },
+};
+</script>
+
+<style></style>
